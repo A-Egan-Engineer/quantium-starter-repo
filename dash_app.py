4c88b26b9e34d6e5493e88bc4b3a24c5c9f6ebdf
Andrew Egan <aeganwork@gmail.com>
2025-12-23 21:06:28 +1100

    Task Three Submit - Adjusted Commenting & Refactored Dash App Source!

--- dash_app.py
+++ dash_app.py
@@ -1,20 +1,22 @@
+# Imports Required Libraries for Dash App
 from dash import Dash, html, dcc
+# Imports Plotly for Data Visualisation
 import plotly.express as px
+#Imporets Pandas for Further Data Manipulation
 import pandas as pd
 
-df = pd.read_csv('data/pink_morsel_sales_data.csv')
+# Assign Pink Morsels Sales Data to DataFrame Variable
+df = pd.read_csv('data/pink_morsel_sales_data.csv') 
 
-app = Dash()
+# Initialises Dash App
+app = Dash(__name__) 
 
+#Dash App Layout Config
 app.layout = ([
     html.H1(children='Pink Morsel Sales Dashboard', style={'textAlign': 'center'}),
     dcc.Graph( id='Pink Morsel Sales Over Time', figure=px.line(df, x='Date', y='Sales', title='Pink Morsel Sales Over Time'))
 ])
 
-@app.callback()
-def update_graph():
-    fig = px.line(df, x='Date', y='Sales', title='Pink Morsel Sales Over Time')
-    return fig
-
+# Runs Dash App
 if __name__ == '__main__':
     app.run(debug=True)
--- pink_morsel_sales_collation.py
+++ pink_morsel_sales_collation.py
@@ -1,25 +1,41 @@
-import pandas as pd # Imports Pandas Library for Data Manipulation
-import glob # Imports Glob Library for File Pattern Matching
+# Imports Pandas Library for Data Manipulation
+import pandas as pd 
+# Imports Glob Library for File Pattern Matching
+import glob 
 
-csv_files = glob.glob('data/daily_sales_data_*.csv')  # Accesses All CSV Files in data Folder with daily_sales_data_ Prefix
+# Accesses All CSV Files in data Folder with daily_sales_data_ Prefix
+csv_files = glob.glob('data/daily_sales_data_*.csv')  
 
-all_data = pd.DataFrame()  # Initialises Empty DataFrame for Future Data Storage
+# Initialises Empty DataFrame for Future Data Storage
+all_data = pd.DataFrame()
 
-# pd.set_option('display.max_rows', None)  # Allows Max Rows to be Displayed in Output (Optional)
+# Allows Max Rows to be Displayed in Output (Optional)
+# pd.set_option('display.max_rows', None)  
 
 for file in csv_files:
-    data = pd.read_csv(file, index_col='Product', parse_dates=['Date'], delimiter=',')  #Loops Thorugh All CSV Files and Reads into all_data DataFrame
-    pink_morsel_data = data.loc[['pink morsel']]  # Filters Data for Pink Morsel Product
-    all_data = pd.concat([all_data, pink_morsel_data])  # Appends All Pink Morsel Data to DataFrame
+    #Loops Thorugh All CSV Files and Reads into all_data DataFrame
+    data = pd.read_csv(file, index_col='Product', parse_dates=['Date'], delimiter=',')  
+    # Filters Data for Pink Morsel Product
+    pink_morsel_data = data.loc[['pink morsel']]  
+    # Appends All Pink Morsel Data to DataFrame
+    all_data = pd.concat([all_data, pink_morsel_data])  
 
-all_data['Date'] = all_data['Date'].dt.strftime('%d-%b-%Y')  # Format Date Column to DD-MMM-YYYY (Australaian Localisation)
-all_data['Sales'] = all_data['Price'].str.replace('$', '').astype(float) * all_data['Quantity']  # Sales = Price * Quantity (Float)
-all_data['Sales'] = '$' + all_data['Sales'].astype(str)  # Format Sales Column with Dollar Sign and Converts to String
+# Format Date Column to DD-MMM-YYYY (Australaian Localisation)
+all_data['Date'] = all_data['Date'].dt.strftime('%d-%b-%Y')  
+# Sales = Price * Quantity (Float)
+all_data['Sales'] = all_data['Price'].str.replace('$', '').astype(float) * all_data['Quantity']  
+# Format Sales Column with Dollar Sign and Converts to String
+all_data['Sales'] = '$' + all_data['Sales'].astype(str)  
 
-print(all_data[['Sales', 'Date', 'Region']])  # Prints Sales, Date, and Region Columns Only (Terminal Output)
+# Prints Sales, Date, and Region Columns Only (Terminal Output)
+print(all_data[['Sales', 'Date', 'Region']])  
 
-all_data['Sales'] = all_data['Sales'].str.replace('$', '').astype(float)  # Converts Sales Column to Float for .Sum Calculation
-total_sales = all_data['Sales'].sum()  # Total Sum of Sales Column
-print(f'Total Sales: ${total_sales:,.2f}' + '\n')  # Prints Total Sales Value with Correct Formatting (Terminal Output)
+# Converts Sales Column to Float for .Sum Calculation
+all_data['Sales'] = all_data['Sales'].str.replace('$', '').astype(float)  
+# Total Sum of Sales Column
+total_sales = all_data['Sales'].sum()  
+# Prints Total Sales Value with Correct Formatting (Terminal Output)
+print(f'Total Sales: ${total_sales:,.2f}' + '\n')  
 
-all_data[['Sales', 'Date', 'Region']].to_csv('data\pink_morsel_sales_data.csv', index=False)  # Creates Pink Morsel Sales Summary Single CSV File (Deliverable of Task Two)
+# Creates Pink Morsel Sales Summary Single CSV File (Deliverable of Task Two)
+all_data[['Sales', 'Date', 'Region']].to_csv('data\pink_morsel_sales_data.csv', index=False)  
